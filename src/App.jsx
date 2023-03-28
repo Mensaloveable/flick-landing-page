2c8d399ba878a82ff9eaee94edc2288188f7211e
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-03-28 13:29:26 +0100

    NavBar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
+import {NavBar} from './components/navbar/NavBar';
 function App() {
-
+    return(
+        <NavBar />
+    )
 }
 
 export default App
---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,25 @@
+import navStyles from './NavBar.module.css'
+import logo from '../../../assets/flick_logo.svg'
+
+export const NavBar = () => {
+    return (
+        <header className={navStyles.navContainer}>
+            <img src={logo} alt='Flick Logo'/>
+            <nav className={navStyles.nav}>
+                <ul className={navStyles.navItems}>
+                    <li className={navStyles.item}><a className={navStyles.navLink} href="#">Business</a></li>
+                    <li className={navStyles.item}><a className={navStyles.navLink} href="#">Pricing</a></li>
+                    <li className={navStyles.item}><a className={navStyles.navLink} href="#">About Us</a></li>
+                    <li className={navStyles.item}><a className={navStyles.navLink} href="#">Developer <span className={navStyles.arrow}>&#8964;</span></a>
+                        <div className={navStyles.dropdownContent}>
+                            <a className={navStyles.navLink} href="#">Overview</a>
+                            <a className={navStyles.navLink} href="#">Api Documentation</a>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+// ToDo: Documentation DropDown
