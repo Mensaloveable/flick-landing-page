8ca1a3259c85d48f764c1e9199a65013a46ebc54
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-03-29 22:01:37 +0100

    Create Body Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import {NavBar} from './components/navbar/NavBar';
+import {NavBar} from "./components/navbar/NavBar";
 import {Footer} from "./components/footer/Footer.jsx";
+import {Body} from "./components/body/Body.jsx";
 
 function App() {
     return (
         <>
             <NavBar/>
+            <Body/>
             <Footer/>
         </>
     )
---
+++ src/components/body/Body.jsx
@@ -0,0 +1,7 @@
+import bodyStyles from './Body.module.css'
+
+export const Body = () => {
+    return (
+        <div className={bodyStyles.pageBody}></div>
+    )
+}
