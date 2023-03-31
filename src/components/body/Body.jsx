75253f68fe73864c150a75f837ae95a8d5bd253f
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-03-31 15:26:54 +0100

    Create JoinFlick component and Review component

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -2,6 +2,8 @@ import bodyStyles from './Body.module.css'
 import {Hero} from "./UI/content/hero/Hero.jsx";
 import {Content} from "./UI/content/Content.jsx";
 import {DownloadApp} from "./UI/content/downloadApp/DownloadApp.jsx";
+import {Review} from "./UI/content/reviews/Review.jsx";
+import {JoinFlick} from "./UI/content/joinFlick/JoinFlick.jsx";
 
 export const Body = () => {
     return (
@@ -9,6 +11,8 @@ export const Body = () => {
             <Hero />
             <Content />
             <DownloadApp />
+            <Review />
+            <JoinFlick />
         </div>
     )
 }
