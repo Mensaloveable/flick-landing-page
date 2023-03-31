6a71bc195dcddcb4390fcde5f5108e2901a2fd2c
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-03-31 07:08:18 +0100

    Create FastPayment component and FlickBanner component

---
+++ src/components/body/UI/content/BannerComponent/FastPayment.jsx
@@ -0,0 +1,20 @@
+import bannerStyle from './Banner.module.css';
+import fastPayment from '../../../../../../assets/payFaster.svg'
+
+export const FastPayment = () => {
+    return (
+        <div className={`${bannerStyle.section} ${bannerStyle.right}`}>
+            <div className={bannerStyle.rightContent}>
+                <div className={`${bannerStyle.animated} ${bannerStyle.fadeInUp}`}>
+                    <h1>Pay anyone 7x faster</h1>
+                    <p>Pay friends, businesses and family, no matter what bank account they use. They get credited
+                        instantly and securely.</p>
+                </div>
+
+            </div>
+            <div className={`${bannerStyle.animated} ${bannerStyle.rightSlide}`}>
+                <img src={fastPayment} alt={'Fast Payment'}/>
+            </div>
+        </div>
+    )
+}
--- src/components/body/UI/content/Content.jsx
+++ src/components/body/UI/content/Content.jsx
@@ -1,10 +1,14 @@
 import contentStyle from './Content.module.css'
 import {Trust} from "./trustComponent/Trust.jsx";
+import {FlickBanner} from "./BannerComponent/FlickBanner.jsx";
+import {FastPayment} from "./BannerComponent/FastPayment.jsx";
 
 export const Content = () => {
     return(
         <div className={contentStyle.main}>
             <Trust />
+            <FlickBanner />
+            <FastPayment />
         </div>
     )
 }
